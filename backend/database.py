46ebfe2b8ab206d5a5e65a5a7c7c04707f0989cd
anyagixx <anyagixx@yandex.ru>
2026-03-19 23:43:26 +0300

    v1.0.5 - Fix database.py SQLModel Session
    - Use SQLModel Session instead of SQLAlchemy sessionmaker
    - Fixes AttributeError: 'Session' object has no attribute 'exec'

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Session, create_engine
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./vpn_manager.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 
 def init_db():
-    """Инициализация базы данных"""
     SQLModel.metadata.create_all(engine)
 
-
 def get_db():
-    """Получение сессии базы данных"""
-    db = SessionLocal()
+    db = Session(engine)
     try:
         yield db
     finally:
